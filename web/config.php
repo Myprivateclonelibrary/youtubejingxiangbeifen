6f2d39f8d909e6850de5b42328ccfca57bcf30e3
wumingshi0531 <35898156+wumingshi0531@users.noreply.github.com>
2018-06-23 09:50:24 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
+define('APIKEY', 'AIzaSyBzdueQ4wHbeHkL6jBqw5th7epIZ1teOSs');//这里改成您的api key
 define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('SITE_NAME', 'YouCCbe');//网站名字
+define('TITLENAME', 'YouCCbe');//网站名字
+define('EN2DEKEY', 'AnQUPCC1EF435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', '849846@QZ.com');//邮箱
 ?>
