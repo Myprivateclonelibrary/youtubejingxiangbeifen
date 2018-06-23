6dea5151d39fe1d799b08dec33ea5e24cd2a320b
wumingshi0531 <35898156+wumingshi0531@users.noreply.github.com>
2018-06-23 10:03:32 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBzdueQ4wHbeHkL6jBqw5th7epIZ1teOSs');//这里改成您的api key
-define('GJ_CODE', 'US');//国家代码
+define('GJ_CODE', 'HK');//国家代码
 define('SITE_NAME', 'YouCCbe');//网站名字
 define('TITLENAME', 'YouCCbe');//网站名字
-define('EN2DEKEY', 'AnQUPCD1EF435e67CczDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '849846@125.com');//邮箱
+define('EN2DEKEY', 'AnQUczDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', '8442349846@125.com');//邮箱
 ?>
